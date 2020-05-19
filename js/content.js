0a516fbf0a95ea3fce40d46f9f51d42e7afb11f9
Arthur <Strevitz@github.com>
2020-05-19 11:09:23 +0200

    commit master

---
+++ js/content.js
@@ -0,0 +1,18 @@
+    // hidden search input
+    
+   /* $( '#main' ).hide();
+
+    $( "#eggplant" ).hover(function() {
+
+        $( '#main' ).show( 1000 );
+
+    });
+
+$('.row-1').mouseenter(function(){
+    $(this).toggleClass("bg-success");
+})
+.mouseleave(function(){
+    $(this).toggleClass("bg-success");  
+    })
+
+*/
